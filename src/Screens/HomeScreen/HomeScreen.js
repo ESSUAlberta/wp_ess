33db5787f85821d03f938b9c5cb2f3619505264b
Mohammad Al-Ahdal <mohammad@enggmomo.me>
2018-12-10 22:04:09 -0700

    updated home to include who we are section

--- src/Screens/HomeScreen/HomeScreen.js
+++ src/Screens/HomeScreen/HomeScreen.js
@@ -6,7 +6,7 @@ import 'react-animated-slider/build/horizontal.css';
 
 const sliderContent = [
     { title: "Take a Break", image: `${require('../../assets/homeSlider/952.jpg')}`, description: "Engineering can be Stressful. Join us for some board games!", link: null },
-    { title: "Engg Ball", link: "/EnggBall/2018/eventbrite" }
+    // { title: "Engg Ball", link: "/EnggBall/2018/eventbrite" }
 ]
 //available properties: title, description, link (to + text), image (css type)
 
@@ -16,30 +16,47 @@ export class HomeScreen extends React.Component {
     }
     render() {
         return (
-            <div key={"homeContent"}>
+            <div className="homeContent" key={"homeContent"}>
                 {/* Big News Slider */}
-                <Slider infinite autoplay={3000}>
-                    {sliderContent.map((content, index) => {
-                        return (
-                            <div className="sliderSlide"
-                                onClick={content.link ? () => { window.location = content.link } : undefined}
-                                style={{
-                                    cursor: content.link ? "pointer" : undefined,
-                                    backgroundPosition: "50% 50%",
-                                    backgroundSize: "cover",
-                                    backgroundImage: content.image ? `linear-gradient(#FFFFFFFF, #FFFFFF00), url(${content.image})` : undefined,
-                                }}>
-                                <div>
-                                    {content.title ? <h2>{content.title}</h2> : null}
-                                    {content.description ? <div>{content.description}</div> : null}
+                <div className="bigNewsSlider">
+                    <Slider infinite autoplay={3000}>
+                        {sliderContent.map((content, index) => {
+                            return (
+                                <div className="sliderSlide"
+                                    onClick={content.link ? () => { window.location = content.link } : undefined}
+                                    style={{
+                                        cursor: content.link ? "pointer" : undefined,
+                                        backgroundPosition: "50% 50%",
+                                        backgroundSize: "cover",
+                                        backgroundImage: content.image ? `linear-gradient(#FFFFFFFF, #FFFFFF00), url(${content.image})` : undefined,
+                                    }}>
+                                    <div>
+                                        {content.title ? <h2>{content.title}</h2> : null}
+                                        {content.description ? <div>{content.description}</div> : null}
+                                    </div>
                                 </div>
-                            </div>
-                        );
-                    })}
-                </Slider>
+                            );
+                        })}
+                    </Slider>
+                </div>
+
+
                 {/* Who we are */}
+                <div className="whoWeAre">
+                    <h1>What is the Engineering Student's Scoiety?</h1>
+                    <p>The Engineering Students’ Society of the University of Alberta is a non-profit organization founded in 1919. Consisting of over 60 titled positions, the ESS advocates for the 4200 engineering students’ interests at the faculty and university levels, offers academic and professional development opportunities, and builds community among engineering students.</p>
+                </div>
+
+
                 {/* What we offer */}
+                <div className="whatWeOffer">
+                </div>
+
+
                 {/* Social Media Feed */}
+                <div className="socialMediaFeed">
+                    
+                </div>
             </div>
 
         );
