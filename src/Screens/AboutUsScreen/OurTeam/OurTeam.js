9825cfcbe704f8c2ae5d40d3efff21597c30b2be
elisajdu <elisajdu@gmail.com>
2019-02-20 20:38:44 -0700

    team page

--- src/Screens/AboutUsScreen/OurTeam/OurTeam.js
+++ src/Screens/AboutUsScreen/OurTeam/OurTeam.js
@@ -1,13 +1,9 @@
-import React from 'react';
+import React from "react";
 
 class OurTeam extends React.Component {
-    render() {
-        return (
-            <div className="aboutUsOurTeam">
-
-            </div>
-        );
-    }
+  render() {
+    return <div className="aboutUsOurTeam">team</div>;
+  }
 }
 
 export default OurTeam;
