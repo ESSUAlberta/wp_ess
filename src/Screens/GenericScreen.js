d4bb3bca5fbd7763ceb503c9ca5b4a96b074e15f
Mohammad Al-Ahdal <mo@mohammad.dev>
2019-07-07 23:50:48 -0600

    add generic page routing, cases, and a 404 page

--- src/Screens/GenericScreen.js
+++ src/Screens/GenericScreen.js
@@ -1,40 +1,59 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { Header, Footer } from "../Components";
+import { Layout, Button } from '@essua/v1-sticks'
+import { Redirect } from 'react-router-dom'
 
 export class GenericScreen extends Component {
-    static propTypes = {
 
+    state = {}
+
+    componentDidMount() {
+        console.log(`state route: dynamicpage.${window.location.pathname}`)
+        global.subscribe(`dynamicpage.${window.location.pathname}`, `GenericScreenPage`, this.forceUpdate.bind(this))
+        global.ESS_API({
+            uri: 'getpage',
+            method: 'post',
+            bodyData: { path: window.location.pathname },
+            stateName: `dynamicpage.${window.location.pathname}`
+        })
     }
 
     render() {
-        return (
-            <div>
-                Generic Screen
-            </div>
-        )
-    }
-}
 
-function withHeaderAndFooter(Component) {
-    return class includeHeaderComponent extends React.Component {
-        render() {
-            return (
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "space-between",
-                        minHeight: "100vh"
-                    }}
-                >
-                    <Header />
-                    <Component />
-                    <Footer />
-                </div>
-            )
-        };
-    };
+        let data = global.getState(`dynamicpage.${window.location.pathname}`)
+
+        if (data && !data.dirty) {
+            return (this.renderPage(data))
+        } else {
+            return (<div>Loading Screen Place Holder</div>);
+        }
+
+
+    }
+
+    renderPage(data) {
+        if (data.code === 404) {
+            return (this.render404());
+        } else {
+            return (this.renderContent(data));
+        }
+    }
+
+    render404() {
+        return (
+            <div className="ess_error404">
+                <h1>How did we wind up here?</h1>
+                <p>This page does not exist - how about we take you back home?</p>
+                <Button size="large" theme="light" style="solid" onClick={() => { this.setState({ goHome: true }) }}>Go Home</Button>
+                {this.state.goHome ? <Redirect to="/" /> : null}
+            </div>
+        );
+    }
+
+    renderContent(data) {
+        return (
+            <Layout type={data.type} data={data.content} />
+        );
+    }
 }
 
 export default GenericScreen
